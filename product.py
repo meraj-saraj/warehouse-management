e8ef84f2e5fa3fc72639ee8c8efc9bcfc6617bc6
saraj <merajm9983@gmail.com>
2026-08-27 08:07:27 +0330

    Create Warehouse class

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
-
 class Product:
     """Represent a single product in the warehouse inventory."""
+
     def __init__(
         self,
         product_name: str,
@@ -9,7 +9,7 @@ class Product:
         minimum_stock: int,
         description: str | None = None,
         product_id: int | None = None,
-    )->None:
+    ) -> None:
         """
         Initialize a product object.
 
@@ -20,7 +20,7 @@ class Product:
             minimum_stock: The product minimum_stock.
             description: The product description.
             product_id: Unique product ID.
-        
+
         """
         self.product_id = product_id
         self.product_name = product_name
@@ -36,5 +36,5 @@ class Product:
             f"description = {self.description!r})"
         )
 
-p1=Product("laptop","0012_LP",2300,100)
-print(p1)
+
+
